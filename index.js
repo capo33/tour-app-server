a12c02b15ca2d81b5234384e604c01aa8bae668b
Mohamed <s2000588@edu.bc.fi>
2023-01-30 20:00:24 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
 
 import connectDB from "./config/db.js";
@@ -20,6 +21,16 @@ app.use(cors());
 // Routes
 app.use("/api/v1/users", userRouter); // http://localhost:5000/api/v1/users/signup
 app.use("/api/v1/tours", tourRouter); // http://localhost:5000/api/v1/tour
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Connect to MongoDB
 connectDB();
